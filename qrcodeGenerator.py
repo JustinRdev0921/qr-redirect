ecbe4d287078fc152c74ca8d19729e3ed0d3f22c
JUSTIN ROSADO <jrosado@ecuacopia.com>
2025-06-26 10:09:57 -0500

    second commit

--- qrcodeGenerator.py
+++
@@ -1,7 +0,0 @@
-import qrcode
-
-# URL que nunca cambia (apunta al archivo redirect.html)
-url_fija = "https://misitio.github.io/redirect.html"
-
-qr = qrcode.make(url_fija)
-qr.save("qr_dinamico.png")
